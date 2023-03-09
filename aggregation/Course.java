033bdc81340d155b1bb48ff45734eb1a5356ce65
Brianne Guerrero <brianne_guerrero@Briannes-Air.lan>
2023-03-09 00:53:56 -0800

    added aggregation

---
+++ aggregation/Course.java
@@ -0,0 +1,7 @@
+package CS5800HW1.aggregation;
+
+public class Course 
+{
+    private String cName;
+    
+}
---
+++ polymorphism/ShipsApp.java
@@ -0,0 +1,14 @@
+package CS5800HW1.polymorphism;
+
+public class ShipsApp 
+{
+    public static void main(String[] args) 
+    {
+        int[] ships = new int[3];
+        for( int index = 0; index< ships.length; index++)
+        {
+            ships[index] = 
+        }
+
+    }
+}
