20d7d2664531426037f2e39c55bded68f7feda00
Brianne Guerrero <brianne_guerrero@b98-aruba-authenticated-10-110-159-69.wlan.cpp.edu>
2023-03-09 18:26:46 -0800

    edited Employee.java

--- inheritance/Employee.java
+++ inheritance/Employee.java
@@ -7,13 +7,13 @@ public class Employee
      * Constructor
      * @param firstName first name of employee
      * @param lastName last name of employee
-     * @param ssNumber social security number of employee
+     * @param sSNumber social security number of employee
      */
-    public Employee(String firstName, String lastName, String ssNumber)
+    public Employee(String firstName, String lastName, String sSNumber)
     {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.sSNumber = ssNumber;
+        this.sSNumber = sSNumber;
     }
     /**
      * The getFirstName method returns the employees first name.
@@ -24,6 +24,15 @@ public class Employee
         return firstName;
     }
     /**
+     * The setFirstName method takes the parameter firstName and assigns it to 
+     * the firstName variable.
+     * @param firstName new name to change it to.
+     */
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    /**
      * The getLastName method returns the employees last name. 
      * @return string in the lastName field.
      */
@@ -32,11 +41,24 @@ public class Employee
         return lastName;
     }
     /**
+     * The setLastName method takes the parameter lastName and assigns it to
+     * the lastName variable.
+     * @param lastName new name to change it to.
+     */
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+    /**
      * The getSSNumber method returns the employees social security number.
      * @return string in the sSNumber field.
      */
     public String getSSNumber()
     {
         return sSNumber;
+    }
+    public void setSSNumber(String sSNumber)
+    {
+        this.sSNumber = sSNumber;
     }
 }
