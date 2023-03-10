b5fe024d8b6742d27cfa80f3e8cee656d7849266
Brianne Guerrero <brianne_guerrero@b98-aruba-authenticated-10-110-159-69.wlan.cpp.edu>
2023-03-09 19:02:22 -0800

    edited BaseEmployee.java

--- inheritance/BaseEmployee.java
+++ inheritance/BaseEmployee.java
@@ -2,15 +2,19 @@ package CS5800HW1.inheritance;
 
 public class BaseEmployee extends Employee
 {
-    private String baseSalary;
-    public BaseEmployee(String firstName, String lastName, String sSNumber, String baseSalary)
+    private double baseSalary;
+    public BaseEmployee(String firstName, String lastName, String sSNumber, double baseSalary)
     {
         super(firstName, lastName, sSNumber);
         this.baseSalary = baseSalary;
     }
     
-    public String getBaseSalaray()
+    public double getBaseSalary()
     {
         return baseSalary;
+    }
+    public void setBaseSalary(double baseSalary)
+    {
+        this.baseSalary = baseSalary;
     }
 }
