df99b6af8b2cffce3ff35ef4c6a4eab70d940788
Brianne Guerrero <brianne_guerrero@b98-aruba-authenticated-10-110-159-69.wlan.cpp.edu>
2023-03-09 18:41:04 -0800

    edited HourlyEmployee.java

--- inheritance/HourlyEmployee.java
+++ inheritance/HourlyEmployee.java
@@ -3,17 +3,20 @@ package CS5800HW1.inheritance;
 public class HourlyEmployee extends Employee
 {
     private String wage;
+    private double hours;
     /**
      * 
      * @param firstName first name of employee
      * @param lastName last name of employee
      * @param sSNumber social security of employee
      * @param wage wage of HourlyEmployee
+     * @param hours number of hours worked 
      */
-    public HourlyEmployee(String firstName, String lastName, String sSNumber, String wage)
+    public HourlyEmployee(String firstName, String lastName, String sSNumber, String wage, double hours)
     {
         super(firstName, lastName, sSNumber);
         this.wage = wage;
+        this.hours = hours;
     }
     /**
      * The getWage method returns the wage of the HourlyEmployee.
@@ -28,4 +31,12 @@ public class HourlyEmployee extends Employee
         this.wage = wage;
     }
     
+    public double getHours()
+    {
+        return hours;
+    }
+    public void setHours(double hours)
+    {
+        this.hours = hours;
+    }
 }
