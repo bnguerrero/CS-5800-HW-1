4bf43424c5e5fbb6f418eb7a292d96167f0ff0f7
Brianee <bnguerrero@cpp.edu>
2023-03-11 12:49:23 -0800

    edited textbook.java

--- aggregation/Textbook.java
+++ aggregation/Textbook.java
@@ -4,11 +4,28 @@ public class Textbook
 {
     private String title, author, publisher;
     
+    /**
+     * Constructor initializes the title, author, and publisher.
+     * @param title the textbook's title.
+     * @param author the textbooks's author.
+     * @param publisher the textbooks's publisher. 
+     */
     public Textbook(String title, String author, String publisher)
     {
         this.title = title;
         this.author = author;
         this.publisher = publisher;
+    }
+
+    /**
+     * The copy constructor initializes the object as a copy of another instructor object. 
+     * @param textbook the object to copy.
+     */
+    public Textbook(Textbook textbook)
+    {
+        this.title = textbook.title;
+        this.author = textbook.author;
+        this.publisher = textbook.publisher;
     }
 
     public String getTitle()
@@ -39,5 +56,10 @@ public class Textbook
     public void setPublisher(String publisher)
     {
         this.publisher = publisher;
+    }
+
+    public void display()
+    {
+        System.out.println("Title: " + title + "\nAuthor: " + author + "\nPublisher: " + publisher);
     }
 }
