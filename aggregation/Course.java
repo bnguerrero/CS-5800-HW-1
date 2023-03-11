5db768be743256c7df64042a2aa72bd54e2121ec
Brianee <bnguerrero@cpp.edu>
2023-03-11 13:26:32 -0800

    edited aggregation folder

--- aggregation/Course.java
+++ aggregation/Course.java
@@ -3,10 +3,14 @@ package CS5800HW1.aggregation;
 public class Course 
 {
     private String courseName;
+    private Instructor instructor;
+    private Textbook textbook;
     
-    public Course(String courseName)
+    public Course(String courseName, Instructor instructor, Textbook textbook)
     {
         this.courseName = courseName;
+        this.instructor = new Instructor(instructor);
+        this.textbook = new Textbook(textbook);
     }
 
     public String getCourseName()
@@ -14,8 +18,24 @@ public class Course
         return courseName;
     }
 
-    public void  setCourseName(String courseName)
+    public void setCourseName(String courseName)
     {
         this.courseName = courseName;
     }
+
+    public Instructor geInstructor()
+    {
+        return new Instructor(instructor);
+    }
+
+    public Textbook getTextbook()
+    {
+        return new Textbook(textbook);
+    }
+
+    public void display()
+    {
+        System.out.println("Course name: " + courseName + "\nInstructor info: " + instructor + "\nTextbook info: " + textbook);
+    }
+
 }
