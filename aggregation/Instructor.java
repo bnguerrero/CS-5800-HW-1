47741d2056326127fdb33993a1ded81afae8f22f
Brianee <bnguerrero@cpp.edu>
2023-03-11 12:32:36 -0800

    edited instructor.java

--- aggregation/Instructor.java
+++ aggregation/Instructor.java
@@ -2,13 +2,30 @@ package CS5800HW1.aggregation;
 
 public class Instructor 
 {
+    
     private String firstName, lastName, officeNumber;
 
+    /**
+     * Constructor initializes the first name, last name, and office number.
+     * @param firstName the instructor's first name.
+     * @param lastName the instructor's last name.
+     * @param officeNumber the instructor's office number. 
+     */
     public Instructor(String firstName, String lastName, String officeNumber)
     {
         this.firstName = firstName;
         this.lastName = lastName;
         this.officeNumber = officeNumber;
+    }
+    /**
+     * The copy constructor initializes the object as a copy of another instructor object. 
+     * @param instructor the object to copy.
+     */
+    public Instructor(Instructor instructor)
+    {
+        this.firstName = instructor.firstName;
+        this.lastName = instructor.lastName;
+        this.officeNumber = instructor.officeNumber;
     }
 
     public String getFirstName()
@@ -39,5 +56,10 @@ public class Instructor
     public void setOfficeNumber(String officeNumber)
     {
         this.officeNumber = officeNumber;
+    }
+
+    public void display()
+    {
+        System.out.println("First name: " + firstName + "\nLast name: " + lastName + "\nOffice number: " + officeNumber);
     }
 }
