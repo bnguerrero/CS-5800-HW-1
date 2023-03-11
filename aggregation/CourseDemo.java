8cc293c3ae2e011bb461b04d589f855f18c1a071
Brianee <bnguerrero@cpp.edu>
2023-03-11 15:07:20 -0800

    created CourseDemo.java

---
+++ aggregation/CourseDemo.java
@@ -0,0 +1,11 @@
+package CS5800HW1.aggregation;
+
+public class CourseDemo 
+{
+    public static void main(String[] args) 
+    {
+        Instructor myInstructor = new Instructor("Nima", "Davarpanah", "office 302636");
+
+        Textbook myTextbook = new Textbook("Clean Code", null, null)
+    }
+}
